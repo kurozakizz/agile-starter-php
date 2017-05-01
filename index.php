dfb2d492927ebd3b47a06c94e460e74ba6de6ac2
nuttawoot <nuttawoot.s@app-alive.com>
2017-05-01 12:21:57 +0700

    add php required version

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $core->setTemplateDir('templates/');
 $data = new Dwoo\Data();
 $data->assign('pageTitle', 'PHP Starter for Agile Training');
 $data->assign('pageContent', 'Welcome to agile training, this is just example php project');
+$data->assign('phpVersion', '5.6.30');
 
 // render template
 echo $core->get('index.tpl', $data);
--- templates/index.tpl
+++ templates/index.tpl
@@ -7,6 +7,10 @@
 <body>
     <h1>{$pageTitle}</h1>
     <p>{$pageContent}</p>
+    <h3>Required</h3>
+    <ul>
+        <li>PHP: {$phpVersion} <a href="https://www.apachefriends.org/index.html">XAMPP installation</a> </li>
+    </ul>
 </body>
 
 </html>
